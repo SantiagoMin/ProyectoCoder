027443c07ee94bec7e3c61b60be8ddec36b5ab3c
SantiagoMin <santiago.minatta@gmail.com>
2022-05-22 19:03:55 -0300

    prueba 2

--- Proyectocoder/urls.py
+++ Proyectocoder/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from Proyectocoder.views import saludo
 from Proyectocoder.views import segunda_vista
+from Proyectocoder.views import DiaDeHoy
+from Proyectocoder.views import nombre_persona
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("saludo/",saludo),
-    path('segundavista/', segunda_vista)
+    path('segundavista/', segunda_vista),
+    path("diaDeHoy/", DiaDeHoy),
+    path("nombre_persona/<nombre>/",nombre_persona)
 ]
--- Proyectocoder/views.py
+++ Proyectocoder/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
- 
+import datetime
 def saludo(request):
   return HttpResponse("Hola mundo")
 
 def segunda_vista(request):
   return HttpResponse("Segunda vista")
+
+def DiaDeHoy(request):
+  dia = datetime.datetime.now()
+  texto = "hoy es:{}".format(dia)
+  return HttpResponse(texto)
+
+def nombre_persona(self, nombre):
+  resultado = "Mi nombre es:<br> <br> {}".format(nombre)
+  return HttpResponse(resultado)
